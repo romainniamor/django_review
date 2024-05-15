3c3749da8eb820dccd8f90b766b3e52c32f85ad6
romainniamor <romainnavoret@gmail.com>
2024-05-15 09:25:24 +0200

    register > forms > create registerform and define elements (email, first, last name)

--- my_project/my_app/forms.py
+++ my_project/my_app/forms.py
@@ -1,3 +1,14 @@
 
 
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from django.contrib.auth.models import User
 
+class RegisterForm(UserCreationForm):
+    email = forms.EmailField(label='', max_length=100, required=True, widget=forms.EmailInput(attrs={'placeholder': 'Email Address', 'class': 'form-control'}))
+    first_name = forms.CharField(max_length=100, label='', widget=forms.TextInput(attrs={'placeholder': 'First Name', 'class': 'form-control'}))
+    last_name = forms.CharField(max_length=100, label='', widget=forms.TextInput(attrs={'placeholder': 'First Name', 'class': 'form-control'})))
+
+    class Meta:
+        model = User
+        fields = ['username','first_name', 'last_name', 'email', 'password1', 'password2']
