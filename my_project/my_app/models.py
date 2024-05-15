7e461e6d3d0fefbe4cc990c49baea39d0e7dcdc1
romainniamor <romainnavoret@gmail.com>
2024-05-16 00:01:37 +0200

    model > model > add class customer

--- my_project/my_app/models.py
+++ my_project/my_app/models.py
@@ -1,3 +1,17 @@
+from django.db import models
 
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=80)
+    email = models.EmailField(max_length=100, unique=True)
+    phone = models.CharField(max_length=20)
+    address = models.TextField(max_length=100)
+    city = models.CharField(max_length=100)
+    zipcode = models.CharField(max_length=10)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return (f'{self.first_name} {self.last_name}')
 
 
