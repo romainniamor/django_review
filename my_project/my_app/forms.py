6089bc0d8160c3a8fa1bf39fb68ad7f05e01e504
romainniamor <romainnavoret@gmail.com>
2024-05-15 16:24:54 +0200

    register > forms > overload RegisterForm passwords

--- my_project/my_app/forms.py
+++ my_project/my_app/forms.py
@@ -3,12 +3,28 @@
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
 from django.contrib.auth.models import User
+#import signupForm
+
+
 
 class RegisterForm(UserCreationForm):
-    email = forms.EmailField(label='', max_length=100, required=True, widget=forms.EmailInput(attrs={'placeholder': 'Email Address', 'class': 'form-control'}))
-    first_name = forms.CharField(max_length=100, label='', widget=forms.TextInput(attrs={'placeholder': 'First Name', 'class': 'form-control'}))
-    last_name = forms.CharField(max_length=100, label='', widget=forms.TextInput(attrs={'placeholder': 'First Name', 'class': 'form-control'})))
+    email = forms.EmailField(label='Email Address', max_length=100, required=True, widget=forms.EmailInput(attrs={'placeholder': 'Email Address', 'class': 'form-control'}))
+    first_name = forms.CharField(max_length=100, label='First Name', widget=forms.TextInput(attrs={'placeholder': 'First Name', 'class': 'form-control'}))
+    username = forms.CharField(max_length=30, label='Username', widget=forms.TextInput(attrs={'placeholder': 'Username', 'class': 'form-control'}))
+    last_name = forms.CharField(max_length=100, label='Last Name', widget=forms.TextInput(attrs={'placeholder': 'Last Name', 'class': 'form-control'}))
 
     class Meta:
         model = User
         fields = ['username','first_name', 'last_name', 'email', 'password1', 'password2']
+
+    def __init__(self, *args, **kwargs):
+        super(RegisterForm, self).__init__(*args, **kwargs)
+        self.fields['password1'].widget = forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password' })
+        self.fields['password1'].label = 'Password'
+        self.fields['password1'].help_text = '<ul class="form-text text-muted small"><li>Your password can\'t be too similar to your other personal information.</li><li>Your password must contain at least 8 characters.</li><li>Your password can\'t be a commonly used password.</li><li>Your password can\'t be entirely numeric.</li></ul>'
+
+        self.fields['password2'].widget = forms.PasswordInput(attrs={'class':'form-control','placeholder':'Confirm Password'})
+        self.fields['password2'].label = 'Confirmation Password'
+        self.fields['password2'].help_text = '<span class="form-text text-muted">Enter the same password as before, for verification.</span>'
+
+
