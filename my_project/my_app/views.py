65373c75bd9c76be776b9eda227f278b40b6e187
romainniamor <romainnavoret@gmail.com>
2024-05-15 19:30:58 +0200

    register > views > add register_user

--- my_project/my_app/views.py
+++ my_project/my_app/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render, get_object_or_404
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.shortcuts import redirect
+from .forms import RegisterForm
 
 
 app_name = 'my_app'
+
+
 def home(request):
     if request.method == 'POST':
         username = request.POST['username']
@@ -23,3 +26,25 @@ def logout_user(request):
     logout(request)
     messages.success(request, 'You have successfully logged out')
     return redirect('my_app:home')
+
+
+def register_user(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, 'Account created for ' + username)
+            return redirect('my_app:home')
+    else:
+        form = RegisterForm()
+        return render(request, 'my_app/register.html', {'form': form})
+
+    return render(request, 'my_app/register.html', {'form': form})
+
+
+
+
