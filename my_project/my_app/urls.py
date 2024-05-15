bf73e6aefef430e7234a6514e898a7981668383f
romainniamor <romainnavoret@gmail.com>
2024-05-15 08:54:02 +0200

    register > urls > add register url to my_app urls

--- my_project/my_app/urls.py
+++ my_project/my_app/urls.py
@@ -5,6 +5,7 @@ app_name = 'my_app'
 urlpatterns = [
     path('', views.home, name='home'),
     path('logout/', views.logout_user, name='logout'),
+    path('register/', views.register_user, name='register'),
 
 
 ]
