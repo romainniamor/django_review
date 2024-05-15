9c654af7d4518532de44a8a8ffe1feb4e930b112
romainniamor <romainnavoret@gmail.com>
2024-05-16 01:22:40 +0200

    models > fix bug with models error

--- my_project/my_app/models.py
+++ my_project/my_app/models.py
@@ -1,1 +1,13 @@
+from django.db import models
 
+class Customer(models.Model):
+    first_name = models.CharField(max_length=100)
+    laste_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    address = models.TextField()
+    city = models.CharField(max_length=100)
+    zipcode = models.CharField(max_length=10)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
