5f2acd5e9b374e82cc60c7a07245b37c194ffee9
romainniamor <romainnavoret@gmail.com>
2024-05-16 15:53:49 +0200

    delete_customers > add url

--- my_project/my_app/urls.py
+++ my_project/my_app/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('logout/', views.logout_user, name='logout'),
     path('register/', views.register_user, name='register'),
     path('customer/<int:customer_id>/', views.customer_detail, name='customer_detail'),
+    path('delete_customer/<int:customer_id>/', views.delete_customer, name='delete_customer'),
 
 
 ]
