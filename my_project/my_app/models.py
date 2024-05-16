d5cf8681a1ffc2f825239b26c781ee624cb0b456
romainniamor <romainnavoret@gmail.com>
2024-05-16 11:01:03 +0200

    homepage > add customers table to homepage

--- my_project/my_app/models.py
+++ my_project/my_app/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 class Customer(models.Model):
     first_name = models.CharField(max_length=100)
-    laste_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
     email = models.EmailField()
     address = models.TextField()
     city = models.CharField(max_length=100)
     zipcode = models.CharField(max_length=10)
-    created_at = models.DateTimeField(auto_now_add=True)
+
 
     def __str__(self):
         return self.first_name + ' ' + self.last_name
