9ab77a3afbb8300d89fb87197209493ed9a42fb8
romainniamor <romainnavoret@gmail.com>
2024-05-17 18:16:45 +0200

    NavBar > customer_search url

--- my_project/my_app/urls.py
+++ my_project/my_app/urls.py
@@ -9,7 +9,8 @@ urlpatterns = [
     path('customer/<int:customer_id>/', views.customer_detail, name='customer_detail'),
     path('delete_customer/<int:customer_id>/', views.delete_customer, name='delete_customer'),
     path('add_customer/', views.add_customer, name='add_customer'),
-    path('update_customer/<int:customer_id>/', views.update_customer, name='update_customer')
+    path('update_customer/<int:customer_id>/', views.update_customer, name='update_customer'),
+    path('search_customer/', views.search_customer, name='search_customer'),
 
 
 ]
